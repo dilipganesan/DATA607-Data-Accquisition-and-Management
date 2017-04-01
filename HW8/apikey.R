ec9812dfa83540aadd0afa0cbc9311b2bb85654b
dilipganesan <dilipgan@gmail.com>
2017-04-01 17:04:04 -0400

    DATA607_Home_Work_9
    API access to get data from NYT Website.

---
+++ HW8/apikey.R
@@ -0,0 +1,3 @@
+key="api-key=a0ae17f7ef5841e9aaf98fe372cc8a52"
+techurl="https://api.nytimes.com/svc/topstories/v1/technology.json"
+bookurl="https://api.nytimes.com/svc/books/v3/lists/overview.json"
